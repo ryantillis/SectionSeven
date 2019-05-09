c8ff98095b8153f7d08847b9dfcf29f5c29d2dda
Ziggy Stardust <zig@Judy-Garland.local>
2019-05-09 12:06:00 -0400

    Hamburgers

---
+++ src/Hamburger.java
@@ -0,0 +1,11 @@
+public class Hamburger {
+    private String meat;
+    private String rollType;
+    private double price;
+    private boolean lettuce;
+    private boolean avacado;
+    private boolean tomato;
+    private boolean
+
+
+}
---
+++ src/HealthyBurger.java
@@ -0,0 +1,5 @@
+public class HealthyBurger extends Hamburger {
+    private boolean quinoa;
+    private boolean arugula;
+
+}
